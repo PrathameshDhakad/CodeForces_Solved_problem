3ec66fc4418f5932c58e3c5f7857912586f198cb
PrathameshDhakad <prathameshdhakad@gmail.com>
2026-08-08 10:05:12 +0530

    Add files via upload

---
+++ Codeforces263A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int value = sc.nextInt();
+
+                if (value == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        int moves = Math.abs(row - 2) + Math.abs(col - 2);
+
+        System.out.println(moves);
+    }
+}
